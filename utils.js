a09518ac39ce6e873248952482bff555b18dafd9
kevinfiero <kjf46@cornell.edu>
2020-11-09 18:05:15 -0800

    trails api

--- utils.js
+++ utils.js
@@ -26,5 +26,22 @@ function mungeWeather(weather) {
     }).slice(0,8);
 }
 
+function mungeTrails(trails) {
+    console.log(trails)
+    return trails.trails.map(item=> {
+        return {
+            name: item.name,
+            location: item.location,
+            length: item.length,
+            stars: item.stars,
+            star_votes: item.starVotes,
+            summary: item.summary,
+            trail_url: item.url,
+            conditions: item.conditionDetails,
+            condition_date: item.conditionDate.split(' ')[0],
+            condition_time: item.conditionDate.split(' ')[1]
+        };
+    }).slice(0,8);
+}
 
-module.exports = { mungeMovie, mungeLocation, mungeWeather };
+module.exports = { mungeMovie, mungeLocation, mungeWeather, mungeTrails };
